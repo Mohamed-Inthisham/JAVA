fa9718136714faf548d09b681856cc1e6255572b
Mohamed Inthi <Mohamed.inthisham999@gmail.com>
2023-12-21 12:21:17 +0530

    begining of java

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author inthi
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        int a=5, b=10, c=15;
+        byte e = 1;
+        short t = 23;
+        double x = 23.5d;
+        float y = 43.5f;
+        char k = 'H';
+        Boolean p = true;
+        String name ="Evotech";
+        System.out.println(a +" "+b+" "+c);
+        System.out.println(e);
+        System.out.println(t);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(k);
+        System.out.println(p);
+        System.out.println(name);       
+    }
+    
+}
