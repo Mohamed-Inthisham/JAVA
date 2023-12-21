bea824ef40489c42d1316c9d9387737e33b2dc67
Mohamed Inthi <Mohamed.inthisham999@gmail.com>
2023-12-21 13:16:09 +0530

    Varible declaration and initialization

---
+++ src/javaapplication1/Variables.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author inthi
+ */
+public class Variables {
+    
+    // variable Declaration
+    
+    String food2;
+  
+    
+    // some lines of codes
+    
+     food2="apple";
+}
