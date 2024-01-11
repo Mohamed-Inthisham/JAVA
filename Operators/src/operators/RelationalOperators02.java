c781296d1eea14b0edf0d81e5981651c1c56c0a7
Mohamed-Inthisham <black.devil9b@gmail.com>
2024-01-11 13:42:01 +0530

    relational operators

---
+++ Operators/src/operators/RelationalOperators02.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operators;
+
+/**
+ *
+ * @author isthi
+ */
+public class RelationalOperators02 {
+   
+    public static void main(String[]args){
+        // Greater than >
+        System.out.println(50 > 10); // true
+        System.out.println(50 > 60);
+        
+        // Greter than or equal >= 
+        System.out.println(50 >= 10); //true
+        System.out.println(50 >= 50); //true
+        System.out.println(50 >= 65);
+        
+        // Less than <
+        System.out.println(20 < 60); // true
+        System.out.println(20 < 18);
+        
+        // Less than or equal <=
+        System.out.println(20 <= 40); //true
+        System.out.println(20 <= 20); // true
+        System.out.println(20 <= 16);
+        
+        // equals ==
+        System.out.println(20 == 20); // true
+        System.out.println(20 == 48);
+        
+        
+        // not equals !=
+        System.out.println(50 != 75); // true
+        System.out.println(50 != 50);
+    }
+    
+}
