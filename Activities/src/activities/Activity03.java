48ba48ca75bad2cdd2f6ea0297d36b7e343aee86
Mohamed-Inthisham <black.devil9b@gmail.com>
2024-01-20 14:49:41 +0530

    activities up to 12

--- Activities/src/activities/Activity03.java
+++ Activities/src/activities/Activity03.java
@@ -52,5 +52,109 @@ public class Activity03 {
          
          short value3=Short.parseShort(s);
          System.out.println(value3);
+         
+         //06
+         char letter='V';
+         System.out.println((int) letter);
+         
+         //08
+         int Mark=67;
+         
+         if(Mark < 0){
+             System.out.println("Negative number not valid");
+         }else if(Mark > 0 && Mark <= 44){
+             System.out.println("Failed");
+         }else if (Mark >=45 && Mark <=54){
+             System.out.println("PASSED: C");
+         }else if(Mark >=55 && Mark <=64){
+             System.out.println("PASSED: C+");
+         }else if(Mark >=65 && Mark <= 74){
+             System.out.println("PASSED: B");
+         }else if(Mark >=75 && Mark <= 79){
+             System.out.println("PASSED: B+");
+         }else if(Mark >=80 && Mark <=100){
+             System.out.println("PASSED: A");
+         }else{
+             System.out.println("Invalid number");
+         }
+         
+         //09
+         
+         int Month=1;
+         
+         switch(Month){
+             case 1 :
+                 System.out.println("January");
+                 break;
+             case 2 :
+                 System.out.println("February");
+                 break;
+             case 3 :
+                 System.out.println("March");
+                 break;
+             case 4 :
+                 System.out.println("April");
+                 break;
+             case 5 :
+                 System.out.println("May");
+                 break;
+             case 6 :
+                 System.out.println("June");
+                 break;
+             case 7 :
+                 System.out.println("July");
+                 break;
+             case 8 :
+                 System.out.println("August");
+                 break;
+             case 9 :
+                 System.out.println("September");
+                 break;
+             case 10 :
+                 System.out.println("October");
+                 break;
+             case 11 :
+                 System.out.println("November");
+                 break;
+             case 12 :
+                 System.out.println("December");
+                 break;
+             default :
+                 System.out.println("Invalid Input");
+           
+         }
+         
+         //10
+         
+         int x1=99;
+         if(x1 == 0){
+             System.out.println("x1 is Zero");
+         }else{
+             if(x1 > 0){
+                 System.out.println("x1 is Positive");
+             }else{
+                 System.out.println("x1 is negative");
+             }
+         }
+         
+         //11
+         
+         for(int i=1 ; i<=5 ;i++){
+             int num=10*i;
+             System.out.println("No "+i+": "+num);
+         }
+         
+         //12
+         
+         int num1=1;
+         while(num1<=10){
+             if(num1==3 || num1==7 || num1==8 || num1==9 ){
+                 num1++;
+                 continue;   
+             }
+             System.out.println(num1);
+             num1++;
+             
+         }
     }
 }
