cfcde1208b1f4c6abbab319e54d1325186224dc5
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-19 15:11:28 +0530

    operator types added

--- Operators/src/operators/Operators.java
+++ Operators/src/operators/Operators.java
@@ -14,7 +14,16 @@ public class Operators {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // JAVA has 8 types of operators
+     /*    JAVA has 8 types of operators
+         --------------------------------
+        Unary Operator,
+        Arithmetic Operator,
+        Shift Operator,
+        Relational Operator,
+        Bitwise Operator,
+        Logical Operator,
+        Ternary Operator and
+        Assignment Operator. */
     }
     
 }
