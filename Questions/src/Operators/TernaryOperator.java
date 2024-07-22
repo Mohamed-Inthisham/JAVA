dc453f67703ebba5be33db265db3cd3af21ba1fa
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-22 12:00:48 +0530

    grade problem solved

--- Questions/src/Operators/TernaryOperator.java
+++ Questions/src/Operators/TernaryOperator.java
@@ -3,7 +3,7 @@ package Operators;
 import java.util.Scanner;
 
 public class TernaryOperator {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         /* 1- Write a Java program that takes an integer input and uses a ternary operator to check if it is positive or non-positive (zero or negative). Print the result accordingly.
 
         Scanner scanner= new Scanner(System.in);
@@ -41,5 +41,37 @@ public class TernaryOperator {
         System.out.println(result);
 
          */
+
+        /*
+        4- Write a Java program that uses a ternary operator to classify a person's age into categories: "Child" (age < 13), "Teenager" (age 13-19), or "Adult" (age 20 or older).
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the age");
+        int age=scanner.nextInt();
+
+        String result=(age < 13)? "Child":(age >= 13 && age <= 19)?"Teenager":"Adult";
+        System.out.println(result);
+
+         */
+
+        /*
+        5- Write a Java program that uses a ternary operator to classify a student's grade into categories:
+"Excellent" for grades 90 and above,
+"Good" for grades between 70 and 89,
+"Average" for grades between 50 and 69,
+"Fail" for grades below 50.
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the grade");
+        int grade = scanner.nextInt();
+
+        String result = (grade < 0 || grade > 100) ? "invalid input" :
+                (grade < 50 ) ? "Fail" :
+                        (grade <70) ? "Average" :
+                                (grade<90) ? "Good" : "Excellent";
+        System.out.println(result);
     }
 }
