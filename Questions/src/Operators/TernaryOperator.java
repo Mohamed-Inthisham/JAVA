aa5f46552b644071f4631ee4bcda4b0a73b4fd84
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-22 11:28:15 +0530

    ternary operator even odd problem solved

--- Questions/src/Operators/TernaryOperator.java
+++ Questions/src/Operators/TernaryOperator.java
@@ -17,18 +17,29 @@ public class TernaryOperator {
          */
 
         /*
-        Write a Java program that takes two integers as input and uses a ternary operator to determine and print the maximum of the two.
-         */
+       2- Write a Java program that takes two integers as input and uses a ternary operator to determine and print the maximum of the two.
+
 
         Scanner scanner =new Scanner(System.in);
-
         System.out.println("enter the 1st number");
         int num1=scanner.nextInt();
         System.out.println("enter the 2nd number");
         int num2=scanner.nextInt();
-
         String max=(num1 == num2) ? "numbers are equal":(num1>num2)?"max number is"+num1:"max number is"+num2;
         System.out.println(max);
 
+         */
+
+        /*
+        3- Write a Java program that uses a ternary operator to check if a given integer is even or odd and prints the result.
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=scanner.nextInt();
+
+        String result=(num %2 ==0)? "even number" : "odd number";
+        System.out.println(result);
+
+         */
     }
 }
