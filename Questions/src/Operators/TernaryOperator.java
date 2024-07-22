441e1a6b2162378e448d5313140293c4940d62b2
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-22 11:21:14 +0530

    ternary operator 2nd question solved

--- Questions/src/Operators/TernaryOperator.java
+++ Questions/src/Operators/TernaryOperator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TernaryOperator {
     public static void main(String[]args){
         /* 1- Write a Java program that takes an integer input and uses a ternary operator to check if it is positive or non-positive (zero or negative). Print the result accordingly.
-         */
+
         Scanner scanner= new Scanner(System.in);
 
         System.out.println("Enter a integer number");
@@ -13,6 +13,22 @@ public class TernaryOperator {
 
         String result =(number > 0) ? "positive" : "non-positive";
         System.out.println(result);
+
+         */
+
+        /*
+        Write a Java program that takes two integers as input and uses a ternary operator to determine and print the maximum of the two.
+         */
+
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("enter the 1st number");
+        int num1=scanner.nextInt();
+        System.out.println("enter the 2nd number");
+        int num2=scanner.nextInt();
+
+        String max=(num1 == num2) ? "numbers are equal":(num1>num2)?"max number is"+num1:"max number is"+num2;
+        System.out.println(max);
 
     }
 }
