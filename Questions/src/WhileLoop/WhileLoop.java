adc5d8df9b89b6c8c72e1ca26fd077d9c588354b
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-24 14:43:34 +0530

    while loop - sum of numbers

---
+++ Questions/src/WhileLoop/WhileLoop.java
@@ -0,0 +1,24 @@
+package WhileLoop;
+
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[]args){
+
+//1- Sum of Numbers: Write a program that sums all integers from 1 to a given number n (inclusive). Use a while loop to calculate the sum.
+//
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println("enter the number");
+//        int n=scanner.nextInt();
+//
+//        int sum=0;
+//        int current=1;
+//
+//        while(current <=n){
+//            sum += current;
+//            current++;
+//            System.out.println(sum);
+//        }
+//        System.out.println(sum);
+    }
+}
