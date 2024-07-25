aacc0a64aa52ba46d1dd4bfb76361449cc6f37a5
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-25 10:31:19 +0530

    do while loop

---
+++ Questions/src/WhileLoop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package WhileLoop;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args){
+        int i=1;
+        do{
+            System.out.println("printing" +i);
+            i++;
+        }while(i<=10);
+    }
+}
